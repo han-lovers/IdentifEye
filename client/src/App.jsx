dd3f28c86ae23114249c587ba1c5e752b66936b5
BarbieMunoz <a01659348@tec.mx>
2024-10-25 15:52:34 -0600

    changed the app.jsx so that it is now in a file pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,17 @@
-import typeImage from './assets/images/upload.png';
-import React, { useState } from "react";
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import UploadPhoto from "./pages/UploadPhoto";
 
 import './App.css'
 
 function App() {
 
-  const [showOptions, setShowOptions] = useState(false);
-
-  // muestra options ... true
-  const handleShowOptions = () => {
-    setShowOptions(!showOptions);
-  }
-
   return (
-    <>
-      <div>
-        <main>
-          <div className='card'>
-
-          <img src={typeImage} alt="Type" className="image-style" ></img>
-
-          <p> <strong>Upload your image</strong> </p>
-
-          <button className="Button" onClick={handleShowOptions}> Choose </button>
-          
-          {showOptions && (
-          <div className='listOption'> 
-            <button className="listButtonOption"> Take a photo </button>
-            <button className="listButtonOption"> Upload a photo </button>
-          </div>  
-          )}
-          </div>
-        </main>
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<UploadPhoto />} />
+      </Routes>
+    </Router>
   )
 }
 
