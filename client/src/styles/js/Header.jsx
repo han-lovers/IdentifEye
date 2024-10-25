f29f9e7324684ceaa6b4632f7c602fc2de27175d
Jorge Cadena <jorge-c@Jorges-MacBook-Pro-2.local>
2024-10-25 16:36:11 -0600

    Corrected navigation bar

--- client/src/styles/js/Header.jsx
+++ client/src/styles/js/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Outlet, Link } from 'react-router-dom';
 import "../css/Header.css";
 import Logo from "../../assets/icons/liverpool-logo.svg";
 
@@ -17,22 +18,24 @@ const Header = () => {
     }
 
     return (
-        <nav className="nav-bar">
-            <img src={Logo} width={250} height={50} alt="logo" />
-            <div className="menu" onClick={handleMenuOpen}>
-                <span className="line"></span>
-                <span className="line"></span>
-                <span className="line"></span>
-            </div>
-            <ul className={`list ${menuOpen ? "open" : ""}`}>
-                <li className="nav-item">
-                    <div className="nav-text">Home</div>
-                </li>
-                <li className="nav-item">
-                    <div className="nav-text">Scan</div>
-                </li>
-            </ul>
-        </nav>
+        <>
+            <nav className="nav-bar">
+                <img src={Logo} width={250} height={50} alt="logo" />
+                <div className="menu" onClick={handleMenuOpen}>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+                <ul className={`list ${menuOpen ? "open" : ""}`}>
+                    <li className="nav-item">
+                        <a className="nav-text" href="/">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-text" href="/scan">Scan</a>
+                    </li>
+                </ul>
+            </nav>
+        </>
     );
 };
 
