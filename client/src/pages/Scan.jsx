369fce4b874d86c08d0bb20db9aad8d3633d38c0
Jorge Cadena <jorge-c@Jorges-MacBook-Pro-2.local>
2024-10-25 18:36:05 -0600

    Added upload image feature

--- client/src/pages/Scan.jsx
+++ client/src/pages/Scan.jsx
@@ -1,6 +1,32 @@
-import { React, useState } from "react";
+import { React, useState, useRef } from "react";
 import typeImage from "../assets/icons/upload.png";
 import "../styles/css/Scan.css";
+
+function SingleFileUploader({ setShowImage }) {
+    const [file, setFile] = useState(null);
+
+    const handleFileChange = (e) => {
+        if (e.target.files) {
+            setFile(e.target.files[0]);
+            setShowImage(false);
+        }
+    };
+
+    const handleUpload = async () => {};
+
+    return (
+        <div className="subeFile">
+            <div className="input">
+                <input id="file" type="file" onChange={handleFileChange} />
+            </div>
+            {file && (
+                <button onClick={handleUpload} className="submit">
+                    Submit
+                </button>
+            )}
+        </div>
+    );
+}
 
 function Scan() {
     const [showOptions, setShowOptions] = useState(false);
@@ -8,36 +34,48 @@ function Scan() {
         setShowOptions(!showOptions);
     };
 
+    const [showFile, setShowFile] = useState(false);
+    const [showImage, setShowImage] = useState(true);
+    const uploadFile = useRef(null);
+
+    const handleUploadPhoto = () => {
+        setShowFile(true);
+        setShowImage(false);
+        setShowOptions(false);
+        uploadFile.current.scrollIntoView({ behavior: "smooth" });
+    };
+
     return (
         <>
             <main>
-
                 <div className="card">
-                    <img
-                        src={typeImage}
-                        alt="Type"
-                        className="image-style"
-                    ></img>
-
-                    <p>
-                        {" "}
-                        <strong>Upload your image</strong>{" "}
-                    </p>
-
+                    {showImage ? (
+                        <>
+                            <img
+                                src={typeImage}
+                                alt="Type"
+                                className="image-style"
+                            />
+                            <p>
+                                <strong>Upload your image</strong>
+                            </p>
+                        </>
+                    ) : (
+                        <SingleFileUploader setShowImage={setShowImage} />
+                    )}
                     <button className="Button" onClick={handleShowOptions}>
-                        {" "}
-                        Choose{" "}
+                        Choose
                     </button>
-
                     {showOptions && (
                         <div className="listOption">
                             <button className="listButtonOption">
-                                {" "}
-                                Take a photo{" "}
+                                Take a photo
                             </button>
-                            <button className="listButtonOption">
-                                {" "}
-                                Upload a photo{" "}
+                            <button
+                                className="listButtonOption"
+                                onClick={handleUploadPhoto}
+                            >
+                                Upload a photo
                             </button>
                         </div>
                     )}
