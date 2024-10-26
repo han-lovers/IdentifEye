05ce358187b22a115254c43e6f42378d044430b0
BarbieMunoz <a01659348@tec.mx>
2024-10-25 18:17:06 -0600

    funciona más padre

--- client/src/pages/Scan.jsx
+++ client/src/pages/Scan.jsx
@@ -2,105 +2,80 @@ import { React, useState, useRef } from "react";
 import typeImage from "../assets/icon/upload.png";
 import "../Style/css/Scan.css";
 
-function SingleFileUploader() {
+function SingleFileUploader({ setShowImage }) {
   const [file, setFile] = useState(null);
 
   const handleFileChange = (e) => {
     if (e.target.files) {
       setFile(e.target.files[0]);
+      setShowImage(false);
     }
-};
+  };
 
-const handleUpload = async () => {
-  console.log("MATENME QUIERO HACER PIPI:");
-};
+  const handleUpload = async () => {
+    
+  };
 
-
-return (
-  <div className="subeFile">
-    <div className="input">
-      <input id="file" type="file" onChange={handleFileChange} />
-    </div>
-    {file && (
-        <section>
-          File details:
-          <ul>
-            <li>Name: {file.name}</li>
-            <li>Type: {file.type}</li>
-            <li>Size: {file.size} bytes</li>
-          </ul>
-        </section>
+  return (
+    <div className="subeFile">
+      <div className="input">
+        <input id="file" type="file" onChange={handleFileChange} />
+      </div>
+      {file && (
+        <button onClick={handleUpload} className="submit">
+          Submit
+        </button>
       )}
-
-    {file && (
-      <button 
-        onClick={handleUpload}
-        className="submit"
-      > Submit </button>
-    )}
-  </div>
-);
+    </div>
+  );
 }
 
 function Scan() {
-    const [showOptions, setShowOptions] = useState(false);
-    const handleShowOptions = () => {
-        setShowOptions(!showOptions);
-    };
+  const [showOptions, setShowOptions] = useState(false);
+  const handleShowOptions = () => {
+    setShowOptions(!showOptions);
+  };
 
-    const [showFile, setShowFile] = useState(false);
-    const uploadFile = useRef(null);
+  const [showFile, setShowFile] = useState(false);
+  const [showImage, setShowImage] = useState(true);
+  const uploadFile = useRef(null);
 
-    const handleUploadPhoto = () => {
-      setShowFile(true);
-      setShowOptions(false);
-      uploadFile.current.scrollIntoView({ behavior: "smooth" });
-    };
-      
-    return (
-        <>
-            <main>
+  const handleUploadPhoto = () => {
+    setShowFile(true);
+    setShowImage(false);
+    setShowOptions(false);
+    uploadFile.current.scrollIntoView({ behavior: "smooth" });
+  };
 
-                <div className="card">
-                    <img
-                        src={typeImage}
-                        alt="Type"
-                        className="image-style"
-                    ></img>
-
-                    <p>
-                        {" "}
-                        <strong>Upload your image</strong>{" "}
-                    </p>
-
-                    <button className="Button" onClick={handleShowOptions}>
-                        {" "}
-                        Choose{" "}
-                    </button>
-
-                    {showOptions && (
-                        <div className="listOption">
-                            <button className="listButtonOption">
-                                {" "}
-                                Take a photo{" "}
-                            </button>
-                            <button className="listButtonOption" onClick={handleUploadPhoto}>
-                                {" "}
-                                Upload a photo{" "}
-                            </button>
-                        </div>
-                    )}
-                </div>
-
-                {showFile && (
-                  <div ref={uploadFile}>
-                    <SingleFileUploader />
-                  </div>
-                )}
-
-            </main>
-        </>
-    );
-}
-
-export default Scan;
+  return (
+    <>
+      <main>
+        <div className="card">
+          {showImage ? (
+            <>
+            <img src={typeImage} alt="Type" className="image-style" />
+          <p>
+            <strong>Upload your image</strong>
+          </p>
+          </>
+          ) : (
+            <SingleFileUploader setShowImage={setShowImage} />
+          )}
+          <button className="Button" onClick={handleShowOptions}>
+            Choose
+          </button>
+          {showOptions && (
+            <div className="listOption">
+              <button className="listButtonOption">Take a photo</button>
+              <button className="listButtonOption" onClick={handleUploadPhoto}>
+                Upload a photo
+              </button>
+            </div>
+                      )}
+                      </div>
+                    </main>
+                  </>
+                );
+              }
+              
+              export default Scan;
