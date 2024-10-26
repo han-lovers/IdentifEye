6fca784e1eea415812f289895a40dfad923cb061
BarbieMunoz <a01659348@tec.mx>
2024-10-25 23:47:32 -0600

    diseño bonito obvio, falta hacerlo responsivo y que se pueda scrollear

--- client/src/pages/Results.jsx
+++ client/src/pages/Results.jsx
@@ -4,10 +4,12 @@ import camisa2 from "../assets/icons/camisa2.jpeg";
 import camisa3 from "../assets/icons/camisa3.jpeg";
 import camisa4 from "../assets/icons/camisa4.jpeg";
 import shipping from "../assets/icons/free-delivery.png";
+import stars from "../assets/icons/stars.png";
 import "../styles/css/Results.css";
 
 function Results() {
     return (
+    <div>
       <div className="container">
         <div className="item">
         <img src={camisa1} alt="Imagen 1" />
@@ -26,14 +28,42 @@ function Results() {
                         </label>
                 </div>
                 <div className="itemBlanco"> 
-                        <label>
+                        <lp>
                         $500.00
-                        </label>
+                        </lp>
+                        <img src={stars} alt="Stars" className="small-image" />
                 </div>
                 <div className="itemBlanco"> 
                         <label>
-                        <img src={shipping} alt="Envio gratis" className="small-image" />
+                        <img src={shipping} alt="Free shipping" className="small-image" />
+                        <span>                Free shipping</span>
                         </label>
+                </div>
+            </div>
+        </div>
+    </div>  
+
+    <div className="container">
+        <div className="containerColumn">
+            <div className="itemBlanco2">
+                <label><strong>Info </strong></label>
+            </div>
+            <div className="itemBlanco2">
+                <label>Details</label>
+            </div>
+            <div className="itemBlanco2">
+                <label>Made of</label>
+            </div>
+        </div>
+
+            <div className="containerColumn">
+                <div className="item2">
+                    <label><strong>You might like...</strong></label>
+                </div>
+                <div className="item2">
+                    <img src={camisa2} alt="Imagen 2" className="like-image"/>
+                    <img src={camisa3} alt="Imagen 3" className="like-image"/>
+                    <img src={camisa4} alt="Imagen 4" className="like-image"/>
                 </div>
             </div>
         </div>
